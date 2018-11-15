7af6b8e377ae8d4ac7019b9edbba37dd54ec80a5
SeenivasanSeeni <seeni0424@gmail.com>
2018-11-15 15:49:16 +0530

    map rendering

--- src/Neighborhood.js
+++ src/Neighborhood.js
@@ -3,9 +3,44 @@ import {APIKey} from "./APIKey";
 import './Neighborhood.css'
 
 export  class Neighborhood extends Component{
+    state={
+        query:'',
+        locations:[],
+        mapReady:false
+    }
+
+    constructor(props) {
+        super(props);
+        this.debug=true;
+        this.setState=this.setState.bind(this);
+    }
+
+    initializeMap(){
+        var bangalore= {
+            position: {lat: 12.9716, lng: 77.5946},
+            text: 'Bangalore Main'
+        }
+        var map=new window.google.maps.Map(document.getElementById('map'),{
+            center: bangalore.position,
+            zoom:8
+        })
+    }
+    componentDidMount(){
+        if(this.debug)
+            console.log("Component Did Mount");
+        var script=document.createElement('script');
+        script.src=`https://maps.googleapis.com/maps/api/js?key=${APIKey}`;
+        script.async=true;
+        script.defer=true;
+        script.addEventListener('load',()=>{
+            console.log("Google Maps API script loaded");
+            this.setState({mapReady: true},this.initializeMap())
+        })
+        document.body.appendChild(script);
+    }
     render(){
         return <div className="Neighborhood">
-            <div className="col-sm-20">
+            <div className="col-sm-30 full-height">
                 <input type="text" placeholder="Enter Place Name " />
                 <div className="search-results">
                     <ol>
@@ -13,8 +48,7 @@ export  class Neighborhood extends Component{
                     </ol>
                 </div>
             </div>
-            <div id="map" className="col-sm-80 map"></div>
-
+            <div id="map" className="col-sm-70 map full-height"></div>
         </div>
     }
 }
